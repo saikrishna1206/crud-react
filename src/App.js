204ea1b006f9e5b2b635cfbfaf82fc5ef3356717
sai krishna <saikrishnafi20@gmail.com>
2023-09-23 19:22:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AddUserForm from "./forms/AddUserForm";
+import UserTable from "./tables/UserTable";
+import EditUserForm from "./forms/EditUserForm";
+
 
 function App() {
+
+const usersData = [
+    {id:1,name:'logan',username:'paul'},
+    {id:2,name:'walter',username:'white'},
+    {id:3,name:'Jhon',username:'snow'},
+];
+
+const addUser = (user)=>{
+    user.id = users.length + 1;
+    setUsers([...users,user])
+}
+const deleteUser = (id)=>{
+    setUsers(users.filter((user)=>user.id!==id))
+    setEditing(false);
+}
+
+    const [users,setUsers] = useState(usersData);
+    const [editing,setEditing] = useState(false)
+    
+
+    const initialFormState = {id:null,name:'',username:''}
+
+    const [currentUser,setCurrentUser] = useState(initialFormState);
+
+    const editRow = (user)=>{
+        setEditing(true);
+        setCurrentUser({id:user.id,name:user.name,username:user.username});
+    }
+
+    const updateUser = (id,updatedUser)=>{
+        setEditing(false);
+        setUsers(users.map((user)=>(user.id===id?updatedUser:user)))
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>DASHBOARD</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+            {editing?(<div>
+                <h2>Edit User</h2>
+                <EditUserForm 
+                    setEditing={setEditing}
+                    currentUser={currentUser}
+                    updateUser={updateUser}
+                />
+            </div>):(<div>
+                <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
+          </div>
+            ) 
+        }
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable editRow={editRow} deleteUser={deleteUser} users={users} />
+        </div>
+      </div>
     </div>
   );
 }
